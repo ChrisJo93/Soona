03a7fa80926807356acc0dcd2777c5cba8328f34
Chris Johnson <Johnny.C.Alexander@gmail.com>
2022-09-06 17:01:49 -0500

    returning string but not passing all tests. Not accounting for apostrophes

--- word_count.rb
+++ word_count.rb
@@ -6,3 +6,11 @@ To get started with TDD, see the `README.md` file in your
 `ruby/word-count` directory.
 =end
 
+# Refactor sanitizer with regex to handle all test conditions
+
+def wordCount(string)
+    countHash = string.downcase.split(/[,\s\W\b]+/).tally()
+    puts countHash
+end
+
+wordCount("this 'this' This,don't thing,thing,this!!!! \n:pun:")
